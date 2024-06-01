38efab395e6ec158626512acd384928e3b3f8967
VijayMakkad <vijaymakkad0104@gmail.com>
2024-06-01 21:19:39 +0530

    Search options

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': 'aeaeb47ca0msh043fe1c46ef2854p197c4bjsnafd3355dd367',
+    'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
+  },
+}
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
+
--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,20 +1,47 @@
-import { useState } from "react"
-import { AsyncPaginate } from "react-select-async-paginate"
-const Search = ({onSearchChange}) => {
-    
-    const [search, setSearch] = useState(null)    
+import React, { useState } from 'react'
+import { AsyncPaginate } from 'react-select-async-paginate'
+import { geoApiOptions, GEO_API_URL } from '../../api'
+import './search.css'
 
-    const handleOnChange=(searchData)=>{
-        setSearch(searchData)
-        onSearchChange(searchData)
-    }
+const Search = ({ onSearchChange }) => {
+  const [search, setSearch] = useState(null)
+
+  const loadOptions = async (inputValue) => {
+    return await fetch(
+      `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
+      geoApiOptions
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        return {
+          options: response.data.map((city) => {
+            return {
+              value: `${city.latitude} ${city.longitude}`,
+              label: `${city.name}, ${city.countryCode}`,
+            }
+          }),
+        }
+      })
+  }
+
+  const handleOnChange = (searchData) => {
+    setSearch(searchData)
+    onSearchChange(searchData)
+  }
+
   return (
-    <AsyncPaginate 
-        placeholder="Search for a City"
-        debounceTimeout={600}
-        value={search}
-        onChange={handleOnChange}
-    />
+    <div className="container">
+      <div className="line">
+        <AsyncPaginate
+          placeholder="Search for a City"
+          debounceTimeout={600}
+          value={search}
+          onChange={handleOnChange}
+          loadOptions={loadOptions}
+        />
+      </div>
+    </div>
   )
 }
+
 export default Search
