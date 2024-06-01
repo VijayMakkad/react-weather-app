ec14b5ddbf805e7c9bf07aa423f4687e9cb392e5
VijayMakkad <vijaymakkad0104@gmail.com>
2024-06-01 17:04:47 +0530

    weather App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Search from './components/search/search';
 
 function App() {
+  
+  const handleOnSearchChange = (searchData) => {
+    console.log(searchData)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className='card'>
+      <Search onSearchChange={handleOnSearchChange}/>
+      </div>
     </div>
   );
 }
---
+++ src/components/search/search.js
@@ -0,0 +1,20 @@
+import { useState } from "react"
+import { AsyncPaginate } from "react-select-async-paginate"
+const Search = ({onSearchChange}) => {
+    
+    const [search, setSearch] = useState(null)    
+
+    const handleOnChange=(searchData)=>{
+        setSearch(searchData)
+        onSearchChange(searchData)
+    }
+  return (
+    <AsyncPaginate 
+        placeholder="Search for a City"
+        debounceTimeout={600}
+        value={search}
+        onChange={handleOnChange}
+    />
+  )
+}
+export default Search
